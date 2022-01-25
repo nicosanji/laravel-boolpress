ed727fde052a88834462b61e6556d21ab1b8d922
nicosanji <n.sangiorgio92@gmail.com>
2022-01-25 19:15:14 +0100

    Creata paginazione lato front e back-end

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -20,7 +20,7 @@ class PostController extends Controller
      */
     public function index()
     {
-        $posts = Post::all();
+        $posts = Post::paginate(3);
         return view('post.index', compact('posts'));
     }
 
--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -3,15 +3,20 @@
 @section('content')
     <div class="container">
 
-        <ol class="mt-5">
+        <ul class="mt-5">
             @foreach ($posts as $post)
                 <li class="mb-5">
                     <a href="{{ route('admin.post.show', $post->id) }}">
                         <h3>{{ $post['title'] }}</h3>
                     </a>
+                </li>
             @endforeach
-            </li>
-        </ol>
+        </ul>
+        <div class="d-flex justify-content-center my-4">
+            {!! $posts->links() !!}
+        </div>
+
+
 
     </div>
 @endsection
